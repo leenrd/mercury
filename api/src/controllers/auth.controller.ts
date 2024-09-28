b8125617fcb7e9e412c4f0106e4c93fa966c2fef
Leenard Zarate <lolleenard0@gmail.com>
2024-09-28 11:51:03 +0800

    clean: suth

--- api/src/controllers/auth.controller.ts
+++ api/src/controllers/auth.controller.ts
@@ -6,28 +6,33 @@ import { Request, Response } from "express";
 interface _auth {
   register(req: Request, res: Response): Promise<void>;
   login(req: Request, res: Response): Promise<void>;
-  logout(req: Request, res: Response): Promise<void>;
+  logout(res: Response): Promise<void>;
 }
 
-// repository implementation
+// repository implementation experiment
 export default class AuthController implements _auth {
   constructor() {}
 
-  async register(req: Request, res: Response) {
+  async register(req: Request<{}, {}, auth_dto>, res: Response) {
     try {
       const { username, password } = req.body;
       const new_user = await AuthService.create("user", { username, password });
+
+      if (new_user instanceof Error) {
+        res.status(HTTP_STATUS.BAD_REQUEST).json({ message: new_user.message });
+        return;
+      }
 
       res
         .status(HTTP_STATUS.CREATED)
         .json({ message: "User created successfully", new_user });
     } catch (error) {
-      console.error(error);
-      res.status(500).send("Error creating user: @AuthController/register");
+      console.error("Error creating user: @AuthController/register");
+      res.status(500).send(error);
     }
   }
 
-  async login(req: Request, res: Response) {
+  async login(req: Request<{}, {}, auth_dto>, res: Response) {
     try {
       const { username, password } = req.body;
       const access_token = await AuthService.create("login_token", {
@@ -35,22 +40,29 @@ export default class AuthController implements _auth {
         password,
       });
 
+      if (access_token instanceof Error) {
+        res
+          .status(HTTP_STATUS.BAD_REQUEST)
+          .json({ message: access_token.message });
+        return;
+      }
+
       bakeCookies(res, access_token);
 
       res.status(HTTP_STATUS.OK).json({ message: "User logged in" });
     } catch (error) {
-      console.error(error);
-      res.status(500).send("Error logging in: @AuthController/login");
+      console.error("Error logging in: @AuthController/login");
+      res.status(500).send(error);
     }
   }
 
-  async logout(req: Request, res: Response) {
+  async logout(res: Response) {
     try {
       res.clearCookie("acc_token");
       res.status(HTTP_STATUS.OK).json({ message: "User logged out" });
     } catch (error) {
-      console.error(error);
-      res.status(500).send("Error logging out: @AuthController/logout");
+      console.error("Error logging out: @AuthController/logout");
+      res.status(500).send(error);
     }
   }
 }
---
+++ api/src/types/auth.dto.ts
@@ -0,0 +1,4 @@
+interface auth_dto {
+  username: string;
+  password: string;
+}
