c4f0e1920a997cbff1eb49cd37a2cb0ed41f2915
Leenard Zarate <lolleenard0@gmail.com>
2024-10-17 15:35:28 +0800

    feat(api+ui): overview btm

--- api/src/controllers/asset.controller.ts
+++ api/src/controllers/asset.controller.ts
@@ -1,19 +1,19 @@
 import { Request, Response } from "express";
 import prisma from "@/lib/prisma";
 
+const temp_assets = [
+  { type: "Real Estate", quantity: 173, fill: "#0090FF" },
+  { type: "Public Assets", quantity: 173, fill: "var(--color-edge)" },
+  { type: "Cash", quantity: 700, fill: "#6E56CF" },
+  { type: "Funding", quantity: 173, fill: "#46A758" },
+  { type: "Venture Capital", quantity: 173, fill: "var(--color-safari)" },
+  { type: "other", quantity: 190, fill: "var(--color-other)" },
+];
+
 const getAssets_controller = async (req: Request, res: Response) => {
   try {
     // const user_id = req.id;
     // console.log("user_id", user_id);
-
-    const temp_assets = [
-      { type: "Real Estate", quantity: 173, fill: "#0090FF" },
-      { type: "Public Assets", quantity: 173, fill: "var(--color-edge)" },
-      { type: "Cash", quantity: 700, fill: "#6E56CF" },
-      { type: "Funding", quantity: 173, fill: "#46A758" },
-      { type: "Venture Capital", quantity: 173, fill: "var(--color-safari)" },
-      { type: "other", quantity: 190, fill: "var(--color-other)" },
-    ];
 
     return res.status(200).json(temp_assets);
   } catch (error: any) {
@@ -29,15 +29,6 @@ const getAssetTotal_controller = async (req: Request, res: Response) => {
   try {
     // const user_id = req.id;
     // console.log("user_id", user_id);
-
-    const temp_assets = [
-      { type: "Real Estate", quantity: 173, fill: "#0090FF" },
-      { type: "Public Assets", quantity: 173, fill: "var(--color-edge)" },
-      { type: "Cash", quantity: 700, fill: "#6E56CF" },
-      { type: "Funding", quantity: 173, fill: "#46A758" },
-      { type: "Venture Capital", quantity: 173, fill: "var(--color-safari)" },
-      { type: "other", quantity: 190, fill: "var(--color-other)" },
-    ];
 
     const total_assets = temp_assets.reduce(
       (acc, asset) => acc + asset.quantity,
--- api/src/routes/user.route.ts
+++ api/src/routes/user.route.ts
@@ -6,6 +6,6 @@ const router = Router();
 const { getUser_controller, updateUser_controller } = user_cn;
 
 router.route("/").get(getUser_controller);
-router.route("/profile-update").post(updateUser_controller);
+router.route("/profile/update").post(updateUser_controller);
 
 export default router;
